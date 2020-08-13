5e51b8b301950808d20fa8a740279795820bdf70
Olesia Miller <Olesia_Miller@epam.com>
2020-08-13 15:44:38 +0300

    fixed comments

--- start.js
+++ start.js
@@ -1,15 +1,6 @@
 import TaskList from './TasksList.js';
 import ModalWindow from './ModalWindow.js';
 
-// const tasks = [{
-//   id: 1,
-//   title: 'title',
-//   description: 'description',
-//   isDone: false,
-//   createDate: '14.08.2020',
-//   deadline: '14.08.2020',
-// }];
-
 const taskList = new TaskList();
 const modalWindow = new ModalWindow();
 
